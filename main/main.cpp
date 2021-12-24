dae25a9d0c6d361c62ff170521fc0c59a187d392
gustavobat <gustavoabatistela@gmail.com>
2021-12-24 00:13:55 -0300

    Add first version of argument handling through docopt

--- main/main.cpp
+++ main/main.cpp
@@ -1,16 +1,44 @@
 #include <iostream>
 #include "strings-n-scales.h"
+#include <docopt.h>
 
-int main() {
+static const char USAGE[] =
+        R"(strings-n-scales.
 
-    std::string instrument_strings = "E, B, G#, E, B";
-    std::string notes_of_interest = "D, F, A";
+    Usage: strings-n-scales
+      strings-n-scales notes <n> strings <s>
+      strings-n-scales (-h | --help)
+      strings-n-scales --version
 
-    InstrumentTranscriber transc;
+    Options:
+      -h --help     Show this screen.
+      --version     Show version.
+)";
 
-    transc.SetInstrumentTuning(instrument_strings);
-    transc.SetNotesOfInterest(notes_of_interest);
-    transc.PrintInstrumentNeck();
+int main(int argc, const char** argv)
+{
+    try {
+        std::map<std::string, docopt::value> args
+                = docopt::docopt(USAGE,
+                                 {argv + 1, argv + argc},
+                                 true, // show help if requested
+                                 "strings-n-scales 0.1"); // version string
+
+        if (!args.find("notes")->second.asBool()) throw std::invalid_argument("Empty 'notes' parameter.");
+        if (!args.find("strings")->second.asBool()) throw std::invalid_argument("Empty 'strings' parameter.");
+        std::string instrument_strings = args.find("<s>")->second.asString();
+        std::string notes_of_interest = args.find("<n>")->second.asString();
+
+        InstrumentTranscriber transc;
+
+        transc.SetInstrumentTuning(instrument_strings);
+        transc.SetNotesOfInterest(notes_of_interest);
+        transc.PrintInstrumentNeck();
+    }
+    catch (std::invalid_argument &e) {
+        std::cerr << e.what() << std::endl;
+        return -1;
+    }
 
     return 0;
 }
