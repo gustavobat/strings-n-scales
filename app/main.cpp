7a034272b9f74b9e2302e8e0ff2dc6018571573d
gustavobat <gustavoabatistela@gmail.com>
2021-12-24 00:51:45 -0300

    Substitute explicit types by 'auto' in main

--- app/main.cpp
+++ app/main.cpp
@@ -2,32 +2,28 @@
 #include "strings-n-scales.h"
 #include <docopt.h>
 
-static const char USAGE[] =
-        R"(strings-n-scales.
-
-    Usage: strings-n-scales
-      strings-n-scales notes <n> strings <s>
-      strings-n-scales (-h | --help)
-      strings-n-scales --version
-
-    Options:
-      -h --help     Show this screen.
-      --version     Show version.
-)";
-
-int main(int argc, const char** argv)
-{
+int main(int argc, const char **argv) {
     try {
-        std::map<std::string, docopt::value> args
-                = docopt::docopt(USAGE,
-                                 {argv + 1, argv + argc},
-                                 true, // show help if requested
-                                 "strings-n-scales 0.1"); // version string
+        const auto usage = R"(
+            strings-n-scales
+
+            Usage: strings-n-scales
+              strings-n-scales notes <n> strings <s>
+              strings-n-scales (-h | --help)
+              strings-n-scales --version
+
+            Options:
+              -h --help     Show this screen.
+              --version     Show version.
+        )";
+
+        const auto args = docopt::docopt(usage, {argv + 1, argv + argc}, true, "strings-n-scales 0.1");
 
         if (!args.find("notes")->second.asBool()) throw std::invalid_argument("Empty 'notes' parameter.");
         if (!args.find("strings")->second.asBool()) throw std::invalid_argument("Empty 'strings' parameter.");
-        std::string instrument_strings = args.find("<s>")->second.asString();
-        std::string notes_of_interest = args.find("<n>")->second.asString();
+
+        const auto instrument_strings = args.find("<s>")->second.asString();
+        const auto notes_of_interest = args.find("<n>")->second.asString();
 
         InstrumentTranscriber transc;
 
